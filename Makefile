9d9f9e7e0c7082dfb5b39967194094425264a539
Lyren Brown <lyren.brown@gmail.com>
2012-10-03 02:25:57 -0500

    revert to volume-only. pwm hisses too much.

--- Makefile
+++ Makefile
@@ -5,10 +5,16 @@ ruff.run:
 atari = /c/Documents\ and\ Settings/lybrown/Documents/Altirra.exe
 
 frames = $(shell cd orig; echo *.png | sed s/.png/.ppm.asm/g)
-ruff.obx: $(frames) ruff.u8
+ruff.obx: $(frames) ruff.audc
 
 %.u8: %.wav
 	sox -v 0.7 $< -u -b 8 -r15600 -D $@ dcshift -0.5 remix -
+
+%.raw: %.wav
+	sox -v 0.15 $< -u -b 8 -r15600 -D $@
+
+%.audc: %.raw
+	./raw2audc $< > $@
 
 %.ppm: orig/%.png
 	convert +dither -compress none $< -remap pal.ppm $@
@@ -29,7 +35,7 @@ ruff.obx: $(frames) ruff.u8
 	perl $< > $@
 	
 %.obx: %.asm
-	xasm /l $<
+	xasm /l /d:pwm=0 $<
 
 clean:
 	rm -f *.{obx,atr,lst} *.{tmc,tm2,pgm,wav}.asm *~
--- ruff.asm.pp
+++ ruff.asm.pp
@@ -12,6 +12,7 @@ init
     sta NMIEN
     sta DMACTL
     mva #15 COLPM0
+    ift pwm
     mva #$ff AUDF1
     mva #$ff AUDF2
     mva #$00 AUDF3
@@ -22,6 +23,7 @@ init
     mva #$a0 AUDC4
     mva #$71 AUDCTL
     mva #$01 SKCTL
+    eif
     mva #0 GRACTL
     mva #0 GRAFP0
     mva #0 GRAFP1
@@ -34,54 +36,96 @@ init
     sta WSYNC
     rts
 showframe
+    ift pwm
     mva #$af AUDC1
     mva #$71 AUDCTL
+    els
+    mva $80 AUDC1
+    eif
     mva <dlist DLISTL
     mva >dlist DLISTH
     ldx #0
 image
-    mva audio,x WSYNC
-    sta AUDF1
-    sta STIMER
+    ldy audio,x
+    sta WSYNC
     mva #0 COLBK
     mva #$32 COLPF0
     mva #$72 COLPF1
     mva #$d2 COLPF2
-    :2 inx
-    mva audio-1,x WSYNC
-    sta AUDF1
+    ift pwm
+    sty AUDF1
     sta STIMER
+    els
+    sty AUDC1
+    eif
+    :2 inx
+    ldy audio-1,x
+    sta WSYNC
     mva #0 COLBK
     mva #6 COLPF0
     mva #10 COLPF1
     mva #14 COLPF2
+    ift pwm
+    sty AUDF1
+    sta STIMER
+    els
+    sty AUDC1
+    eif
     cpx #240
     bne image
     ldx #0
 blank
-    mva audio+240,x WSYNC
-    sta AUDF1
+    ldy audio+240,x
+    sta WSYNC
+    :4 mva #0 COLBK
+    :1 nop
+    ift pwm
+    sty AUDF1
     sta STIMER
+    els
+    sty AUDC1
+    eif
     inx
     cpx #[312-240]
     bne blank
+    sty $80
     rts
 freeze
     jsr showframe
     jmp freeze
 bitmap equ [$4000+$10]
-audio equ [bitmap+240*40]
+audio equ [bitmap+240*40+16]
 dlist
     dta $4e,a(bitmap)
-    :203 dta $e
+    :101 dta $e
+    dta $4e,a(bitmap+$1000-$10)
+    :101 dta $e
     dta $4e,a(bitmap+$2000-$10)
     :35 dta $e
     dta $41,a(dlist)
     ini init
-    ;org bitmap
-    ;:10000 dta [<#]
->>> for () {
-    org bitmap
-
+>>> use strict; use warnings;
+>>> my @frames = <ruff*.ppm.asm>;
+>>> splice @frames, 1500;
+>>> sub audio {
+>>>   my ($count) = @_;
+>>>   print "   ins 'ruff.audc',$count*312,312\n";
 >>> }
+>>> my $count = 0;
+>>> for my $frame (@frames) {
+    org bitmap
+>>> print "   icl '$frame'\n";
+    org audio
+>>> audio($count++);
+    ini showframe
+    org audio
+>>> audio($count++);
+    ini showframe
+>>> }
+    org audio
+    ift pwm
+    :312 dta 0
+    els
+    :312 dta $18
+    eif
     ini freeze
